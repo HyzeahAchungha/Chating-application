d4602dc9be2a660cd338b3745de8535d58f61348
Hyzeah Achungha <109292041+HyzeahAchungha@users.noreply.github.com>
2024-02-08 17:47:11 +0100

    Update postRequest.js

--- my-chart/src/Api/postRequest.js
+++ my-chart/src/Api/postRequest.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({baseURL:'http://localhost:5000'})
+const API = axios.create({baseURL:'https://chating-application-nine.vercel.app/'})
 
 export const getTimelinePosts= (id)=> API.get(`/post/${id}/timeline`);
 export const likePost=(id,userId)=>API.put(`post/${id}/like`,{userId:userId})
