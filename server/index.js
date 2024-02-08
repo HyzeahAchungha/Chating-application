55779d17af9aae19fd7daaefdf80a70db28dbcdb
Hyzeah Achungha <109292041+HyzeahAchungha@users.noreply.github.com>
2024-02-08 18:20:51 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,13 +19,14 @@ app.use(express.static('public'))
 app.use('/images', express.static("images"))
 
 //middleware
-app.use(cors(
-	{
-		origin:{https://chating-application-kdpx.vercel.app/auth}
-			methods:{"POST","GET","DELETE"}
-			credentials:true
-	}
-));
+onst cors = require('cors');
+const corsOptions ={
+    origin:'https://chating-application-kdpx.vercel.app/auth', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 dotenv.config()
