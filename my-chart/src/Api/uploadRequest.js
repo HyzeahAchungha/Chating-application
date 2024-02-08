8c0b5e21d8a796def8657c41ee83cc51137709d1
Hyzeah Achungha <109292041+HyzeahAchungha@users.noreply.github.com>
2024-02-08 17:46:35 +0100

    Update uploadRequest.js

--- my-chart/src/Api/uploadRequest.js
+++ my-chart/src/Api/uploadRequest.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({baseURL:'http://localhost:5000'})
+const API = axios.create({baseURL:'https://chating-application-nine.vercel.app/'})
 
 export const uploadImage = (data)=> API.post("/upload",data)
 export const uploadPost = (data)=>API.post("/post/posts/",data)
