903a2555ec5d0da8058948e4d0419604c60661f5
Hyzeah Achungha <109292041+HyzeahAchungha@users.noreply.github.com>
2024-02-08 17:48:38 +0100

    Update AuthRequest.js

--- my-chart/src/Api/AuthRequest.js
+++ my-chart/src/Api/AuthRequest.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({baseURL:'http://localhost:5000'})
+const API = axios.create({baseURL:'https://chating-application-nine.vercel.app/'})
 
 export const logIn= (formData)=>API.post('/auth/login',formData)
 export const signUp= (formData)=>API.post('/auth/register',formData)
