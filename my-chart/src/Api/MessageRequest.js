830f14069692fd4590fb7c7327c5dba72d82a794
Hyzeah Achungha <109292041+HyzeahAchungha@users.noreply.github.com>
2024-02-08 17:47:42 +0100

    Update MessageRequest.js

--- my-chart/src/Api/MessageRequest.js
+++ my-chart/src/Api/MessageRequest.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({baseURL:'http://localhost:5000'})
+const API = axios.create({baseURL:'https://chating-application-nine.vercel.app/'})
 
 export const getMessages = (id) => API.get(`/message/${id}`);
 export const addMessage = (data)=>API.post('/message/', data)
